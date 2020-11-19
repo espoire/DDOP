cbbc1387674290abc15514f3369ae8f37e2571b7
espoire <a.pergrossi@gmail.com>
2020-11-18 23:01:20 -0500

    JSON directory structure refactor.

--- src/ddop/Settings.java
+++ src/ddop/Settings.java
@@ -8,17 +8,21 @@ public class Settings {
 
 	public static final String REFINEMENT_OUTPUT_DIRECTORY = PROJECT_ROOT + "\\logs";
 	public static final String WORK_DIRECTORY    = PROJECT_ROOT + "\\DDOP Loader";
-	public static final String JSON_DIRECTORY    = PROJECT_ROOT + "\\itemJsons";
 	public static final String SOURCE_DIRECTORY  = WORK_DIRECTORY + "\\src";
 	public static final String OUTPUT_DIRECTORY  = WORK_DIRECTORY + "\\out";
-	public static final String WIKI_ITEMS_JSON                         = JSON_DIRECTORY + "\\wiki.json";
+	public static final String JSON_DIRECTORY    = PROJECT_ROOT + "\\json";
+
+	public static final String WIKI_ITEMS_JSON                         = JSON_DIRECTORY + "\\wiki\\items.json";
 	public static final String MANUAL_ITEMS_JSON                       = JSON_DIRECTORY + "\\manual.json";
 	public static final String SET_BONUS_DEFINITIONS_JSON_DIRECTORY    = JSON_DIRECTORY + "\\setBonus";
-	public static final String NEARLY_FINISHED_BASE_ITEMS_JSON         = JSON_DIRECTORY + "\\nearlyFinishedBaseItems.json";
-	public static final String NEARLY_FINISHED_RECIPE_DEFINITIONS_JSON = JSON_DIRECTORY + "\\nearlyFinishedRecipeDefinitions.json";
-	public static final String NEARLY_FINISHED_COMPLETED_ITEMS_JSON    = JSON_DIRECTORY + "\\nearlyFinishedCompletedItems.json";
-	public static final String SLAVERS_RECIPE_DEFINITIONS_JSON         = JSON_DIRECTORY + "\\slaversRecipeDefinitions.json";
-	public static final String CANNITH_RECIPE_DEFINITIONS_JSON         = JSON_DIRECTORY + "\\cannithRecipeDefinitions.json";
+
+	public static final String NEARLY_FINISHED_JSON_DIRECTORY          = JSON_DIRECTORY + "\\nearlyFinished";
+	public static final String NEARLY_FINISHED_BASE_ITEMS_JSON         = NEARLY_FINISHED_JSON_DIRECTORY + "\\baseItems.json";
+	public static final String NEARLY_FINISHED_COMPLETED_ITEMS_JSON    = NEARLY_FINISHED_JSON_DIRECTORY + "\\completedItems.json";
+	public static final String NEARLY_FINISHED_RECIPE_DEFINITIONS_JSON = NEARLY_FINISHED_JSON_DIRECTORY + "\\recipeDefinitions.json";
+
+	public static final String SLAVERS_RECIPE_DEFINITIONS_JSON         = JSON_DIRECTORY + "\\slavers\\recipeDefinitions.json";
+	public static final String CANNITH_RECIPE_DEFINITIONS_JSON         = JSON_DIRECTORY + "\\cannith\\recipeDefinitions.json";
 
 	public static final String[] ITEM_SOURCES_JSON = new String[] {
 			WIKI_ITEMS_JSON,
