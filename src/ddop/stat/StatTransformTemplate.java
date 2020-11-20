91a9c3587d09d1bdb71a5c1efaf0935cd1e86169
espoire <a.pergrossi@gmail.com>
2020-11-20 11:52:38 -0500

    NamedStats now unpack at load time, recursively unrolling themselves until they map a source stat directly into fundamental stats without multiple layers of NamedStat indirection.

--- src/ddop/stat/StatTransformTemplate.java
+++ src/ddop/stat/StatTransformTemplate.java
@@ -19,7 +19,7 @@ public class StatTransformTemplate {
      * @param source The original stat which triggered this conversion.
      * @return new Stat
      */
-    public Stat apply(Stat source) {
+    public Stat applyTo(Stat source) {
         String bonusType = this.bonusType;
         if(bonusType == null)
             bonusType = source.bonusType;
@@ -32,5 +32,53 @@ public class StatTransformTemplate {
         }
 
         return new Stat(this.category, bonusType, magnitude);
+    }
+
+    /** As the apply(Stat) method, but transforms another StatTransformTemplate.
+     *
+     * @param source The template to transform.
+     * @return new StatTransformTemplate
+     */
+    public StatTransformTemplate applyTo(StatTransformTemplate source) {
+        String bonusType = this.bonusType;
+        if(bonusType == null)
+            bonusType = source.bonusType;
+
+        Double magnitude;
+        if(this.magnitude == null) {
+            magnitude = source.magnitude;
+        } else {
+            magnitude = this.magnitude;
+        }
+
+        return new StatTransformTemplate(this.category, bonusType, magnitude);
+    }
+
+    public String toString() {
+        String ret = "";
+
+        ret += "\"" + this.category + "\" ";
+
+        if(this.magnitude == null) {
+            ret += "[magnitude]";
+        } else {
+            if(magnitude == (int) (double) magnitude) {
+                ret += " " + (int) (double) magnitude;
+            } else {
+                ret += magnitude;
+            }
+        }
+
+        ret += " (";
+
+        if(this.bonusType == null) {
+            ret += "[bonus type]";
+        } else {
+            ret += this.bonusType;
+        }
+
+        ret += ")";
+
+        return ret;
     }
 }
--- src/ddop/stat/conversions/NamedStat.java
+++ src/ddop/stat/conversions/NamedStat.java
@@ -14,12 +14,15 @@ import java.util.Map;
 
 public class NamedStat {
     private static final Map<String, NamedStat> all = NamedStat.loadJson();
+    private final String name;
     private final List<StatTransformTemplate> conversion;
 
-    private NamedStat(List<StatTransformTemplate> conversion) {
+    private NamedStat(String name, List<StatTransformTemplate> conversion) {
+        this.name = name;
         this.conversion = conversion;
     }
 
+    //region Initialization
     private static Map<String, NamedStat> loadJson() {
         String json = file.Reader.getEntireFile(Settings.NAMED_STAT_DEFINITIONS_JSON);
         Type type = new TypeToken<Map<String, List<StatTransformTemplate>>>() {}.getType();
@@ -27,10 +30,54 @@ public class NamedStat {
 
         Map<String, NamedStat> ret = new HashMap<>();
         for(Map.Entry<String, List<StatTransformTemplate>> entry : fromJson.entrySet())
-            ret.put(entry.getKey(), new NamedStat(entry.getValue()));
+            ret.put(entry.getKey(), new NamedStat(entry.getKey(), entry.getValue()));
+
+        return unpack(ret);
+    }
+
+    //region Unpacking
+    /** Recursively unpacks all NamedStat definitions until they convert directly into basic
+     * stats without referencing another NamedStat along the way. */
+    private static Map<String, NamedStat> unpack(Map<String, NamedStat> toUnpack) {
+        for(NamedStat ns : toUnpack.values())
+            unpack(ns, toUnpack);
+
+        return toUnpack;
+    }
+
+    /** Unpack all the StatTransformTemplates in the provided NamedStat, and replace the
+     * conversion list with the result. */
+    private static void unpack(NamedStat toUnpack, Map<String, NamedStat> statMap) {
+        List<StatTransformTemplate> unpacked = new ArrayList<>();
+        for(StatTransformTemplate stt : toUnpack.conversion)
+            if(stt != null)
+                unpacked.addAll(unpack(stt, statMap));
+
+        toUnpack.conversion.clear();
+        toUnpack.conversion.addAll(unpacked);
+    }
+
+    /** Unpacks the provided StatTransformTemplate, by attempting to recursively apply
+     * NamedStat conversion to the template. */
+    private static List<StatTransformTemplate> unpack(StatTransformTemplate toUnpack, Map<String, NamedStat> statMap) {
+        NamedStat ns = statMap.get(toUnpack.category);
+        List<StatTransformTemplate> ret = new ArrayList<>();
+
+        if(ns == null || ns.conversion.size() == 0) {
+            ret.add(toUnpack);
+        } else {
+            for(StatTransformTemplate template : ns.conversion) {
+                StatTransformTemplate unpacked = template.applyTo(toUnpack);
+
+                List<StatTransformTemplate> fullyUnpacked = unpack(unpacked, statMap);
+                ret.addAll(fullyUnpacked);
+            }
+        }
 
         return ret;
     }
+    //endregion
+    //endregion
 
     public static List<Stat> convertAll(List<Stat> toConvert) {
         List<Stat> converted = new ArrayList<>();
@@ -43,8 +90,8 @@ public class NamedStat {
     /** Returns a List of elementary stats into which the provided compound stat decomposes.
      * If the provided stat does not decompose, it will instead be returned as the sole element.
      *
-     * @param toConvert - The stat to convert.
-     * @return Stat[].
+     * @param toConvert The stat to convert.
+     * @return List&lt;Stat>
      */
     public static List<Stat> convert(Stat toConvert) {
         NamedStat ns = NamedStat.all.get(toConvert.category);
@@ -56,7 +103,7 @@ public class NamedStat {
 
         List<Stat> ret = new ArrayList<>(ns.conversion.size());
         for(StatTransformTemplate template : ns.conversion) {
-            Stat conversion = template.apply(toConvert);
+            Stat conversion = template.applyTo(toConvert);
 
             List<Stat> fullyConverted = convert(conversion);
             ret.addAll(fullyConverted);
@@ -68,5 +115,23 @@ public class NamedStat {
     public static boolean isNamed(Stat s) { return NamedStat.isNamed(s.category); }
     public static boolean isNamed(String s) {
         return NamedStat.all.containsKey(s);
+    }
+
+    public String toString() {
+        StringBuilder ret = new StringBuilder();
+
+        ret.append(this.name).append(" -> [");
+
+        boolean isFirst = true;
+        for(StatTransformTemplate stt : this.conversion) {
+            if(!isFirst) ret.append(", ");
+            isFirst = false;
+
+            ret.append(stt);
+        }
+
+        ret.append("]");
+
+        return ret.toString();
     }
 }
